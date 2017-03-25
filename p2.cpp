a0dc95dc408ef12394e337ddb23b25c182658361
viziad <mtbeale@hotmail.com>
2017-03-25 16:12:20 -0400

    Update p2.cpp
    Added menu window(exit works for now), typing text and arrow keys work for the most part
    todo: clean code/reorganize, open file, save file, save file as, errors,

--- p2.cpp
+++ p2.cpp
@@ -1,13 +1,14 @@
 #include "Editor.h"
 #include "Buffer.h"
 #include <ncurses.h>
+#include <cstring>
+#include <cstdlib>
 
 using namespace std;
 
 void init_ncurses();
-void updateText();
-void input(int);
 static void quit();
+void showmenu(void);
 
 char * fname;
 int trow, tcol; // rows and cols in screen
@@ -15,204 +16,160 @@ int crow, ccol; // coordinates of cursor
 int ppos; // pad position
 int psize;
 int lines;
-bool doExit = false;
+int ch;
 Editor ed;
 Buffer * cont;
 WINDOW * filePad; // file content pad
-
+WINDOW * text_border;
+WINDOW * menu;
 // TODO: argument/error handling
 int main(const int argc, char * argv[]) {
-    if (argc > 1) {
-        fname = argv[1];
-        ed = Editor(fname); // new editor object using existing file
-    } else {
-        ed = Editor(); // new editor object using new file
-    }
+  if (argc > 1) {
+    fname = argv[1];
+    ed = Editor(fname); // new editor object using existing file
+  } else {
+    ed = Editor(); // new editor object using new file
+  }
 
-    init_ncurses();
+  init_ncurses();
 
-    ppos = 0;
-    crow = 0;
-    ccol = 0;
-    updateText();
-    prefresh(filePad, ppos, 0, 2, 1, trow-3, tcol-2);
-    // main program loop
-    while (!doExit) {
-    	updateText();
-    	int ch = wgetch(filePad);
-    	input(ch);
-    }
-
-    quit();
+  //main program loop(press F2 to exit,F1 for menu)
+  while (ch != KEY_F(2)) {
+   ch = wgetch(text_border);
+   wrefresh(text_border);
+   switch(ch) {
+   case KEY_LEFT:
+     getyx(text_border,ccol,crow);
+     crow--;
+     wmove(text_border,ccol,crow);
+     break;
+   case KEY_RIGHT:
+     getyx(text_border,ccol,crow);
+     crow++;
+     wmove(text_border,ccol,crow);
+     break;
+   case KEY_UP:
+     getyx(text_border,ccol,crow);
+     ccol--;
+     wmove(text_border,ccol,crow);
+     break;
+   case KEY_DOWN:
+     getyx(text_border,ccol,crow);
+     ccol++;
+     wmove(text_border,ccol,crow);
+     break;
+   case KEY_F(1):
+     showmenu();
+     break;
+   default:
+   waddch(text_border,ch);
+   break;
+   }
+  }
+ quit();
 }
 
 void init_ncurses() {
-    char header[] = "CSCI 1730 Text Editor";
+  char header[] = "CSCI 1730 Text Editor";
 
-    initscr();
-    noecho();
-    cbreak();
-    keypad(stdscr, true);
+  initscr();
+  noecho();
+  cbreak();
+  keypad(stdscr,true);
 
-    getmaxyx(stdscr, trow, tcol);
+  getmaxyx(stdscr, trow, tcol);
 
-    mvaddstr(0, 0, "F1: MENU  F2: QUIT");
-    mvaddstr(0, (tcol-strlen(header))/2, header);
-    const char * filename = ed.getFilename().c_str(); // convert filename string to const char *
-    mvaddstr(trow-1, 0, filename);
-
-
-    filePad = newpad(10000, tcol-2);
-    keypad(filePad, true);
-    nodelay(filePad, true);
-    psize = trow-5;
-
-    refresh();
-    
-    WINDOW * text_border; // new window on top of the stdscr
-    text_border = newwin(LINES-2,COLS,1,0); // setting dimensions of text_area window
-    box(text_border, 0, 0); // window border
-    wrefresh(text_border);
-    delwin(text_border); // deallocate
-    endwin();
-}
-
-void updateText() {
-    cont = ed.getBuffer();
-    lines = cont->text.size()-1;
-
-    for (int i = 0; i < lines; i++) {
-        mvwprintw(filePad, i, 0, cont->text[i].c_str());
-    }
-    wmove(filePad, crow, ccol);
-    prefresh(filePad, ppos, 0, 2, 1, trow-3, tcol-2);
-}
-
-void input(int ch) {
-    switch(ch) {
-    	case KEY_F(2):
-    		doExit = true;
-    		break;
-        case KEY_UP:
-            if (crow > 0) {
-                if (crow == ppos) {
-                    ppos--;
-                }
-                crow--;
-                if (ccol >= cont->text[crow].length()) {
-                    ccol = cont->text[crow].length();
-                }
-            }
-            wmove(filePad, crow, ccol);
-            break;
-        case KEY_DOWN:
-            if (crow < lines-1) {
-                if (crow == ppos+psize) {
-                    ppos++;
-                }
-                crow++;
-                if (ccol >= cont->text[crow].length()) {
-                    ccol = cont->text[crow].length();
-                }
-            }
-            wmove(filePad, crow, ccol);
-            break;
-        case KEY_LEFT:
-            if (ccol > 0) {
-                ccol--;
-            }
-            wmove(filePad, crow, ccol);
-            break;
-        case KEY_RIGHT:
-            if ((ccol < tcol) && (ccol < cont->text[crow].length())) {
-                ccol++;
-            }
-            wmove(filePad, crow, ccol);
-            break;
-    }
-    prefresh(filePad, ppos, 0, 2, 1, trow-3, tcol-2);
-}
-
-static void quit() {
-    endwin();
-    exit(0);
-}
-
-//I commented this out because i'm still working on implementing it differently,
-//but the code should be mostly correct
-
-/*
-void create_menu() {
-
-  // Menu choices
-  const char *choices[] = {
-    "Open",
-    "Save",
-    "Save As",
-    "Exit",
-  };
-
-  ITEM **my_items;
-  int c;
-  MENU *my_menu;
-  WINDOW *my_menu_win;
-  int n_choices, i;
-
-  //creates/allocates choices to the menu
-  n_choices = ARRAY_SIZE(choices);
-  my_items = (ITEM **)calloc(n_choices, sizeof(ITEM *));
-  for(i = 0; i < n_choices; ++i)
-    my_items[i] = new_item(choices[i]);
-
-  //creates the actual menu
-  my_menu = new_menu((ITEM **)my_items);
-
-  //creates the window for the menu
-  my_menu_win = newwin(10,40,4,4);
-  keypad(my_menu_win,TRUE);
+  mvaddstr(0, 0, "F1: MENU  F2: QUIT");
+  mvaddstr(0, (tcol-strlen(header))/2, header);
+  const char * filename = ed.getFilename().c_str(); // convert filename string to const char
+  mvaddstr(trow-1, 0, filename);
 
 
-  set_menu_win(my_menu, my_menu_win);
-  set_menu_sub(my_menu, derwin(my_menu_win, 6, 38, 3, 1));
-
-  //Menu mark is *(aka * will appear next to the selected menu option)
-  set_menu_mark(my_menu, " * ");
-
-  //Print a border around the menu window
-  box(my_menu_win, 0, 0);
-  print_in_middle(my_menu_win, 1, 0, 40, "My Menu");
-  mvwaddch(my_menu_win, 2, 0, ACS_LTEE);
-  mvwhline(my_menu_win, 2, 1, ACS_HLINE, 38);
-  mvwaddch(my_menu_win, 2, 39, ACS_RTEE);
+  filePad = newpad(10000, tcol-2);
+  keypad(filePad, true);
+  nodelay(filePad, true);
+  psize = trow-5;
   refresh();
 
-  //post menu to screen
-  post_menu(my_menu);
-  wrefresh(my_menu_win);
+
+  text_border = newwin(LINES-2,COLS,1,0); // setting dimensions of text_area
+  keypad(text_border,TRUE);
+  cbreak();
+  box(text_border, 0, 0); // window border
+ wborder(text_border,'|','|','-','-','-','-','-','-');
+ wmove(text_border,1,1);
+ wrefresh(text_border);
 
 
-  while((c = wgetch(my_menu_win)) != KEY_F(1))
-    {       switch(c)
-        {case KEY_DOWN:
-            menu_driver(my_menu, REQ_DOWN_ITEM);
-            break;
-  case KEY_UP:
-          menu_driver(my_menu, REQ_UP_ITEM);
-          break;
-        }
-      wrefresh(my_menu_win);
-    }
+ //making the menu window
+ menu = newwin(10,40,4,4);
+ box(menu,0,0);
+ wborder(menu,'|','|','-','-','-','-','-','-');
+ mvwhline(menu,2,1,'-',38);
+ refresh();
+ mvwaddstr(menu,1,7,"My Menu");
+ keypad(menu,true);
 
-  //free previously allocated memory
-  unpost_menu(my_menu);
-  free_menu(my_menu);
-  for (i =0; i < n_choices; i++)
-    free_item(my_items[i]);
+
+}
+
+
+static void quit() {
+  delwin(text_border);
+  delwin(menu);
   endwin();
+  exit(0);
 }
+//fills the menu window with options, and allows navigation/selection
+void showmenu(void) {
+  string choices[4] = {"Open", "Save", "Save As", "Exit"};
+  int choice;
+  int highlight = 0;
+  touchwin(menu);
+ wrefresh(menu);
 
+  while (1) {
+    for (int i = 0; i < 4; i++) {
+      if (i == highlight)
+        wattron(menu, A_REVERSE);
+      mvwprintw(menu, i+3, 1, choices[i].c_str());
+      wattroff(menu, A_REVERSE);
+    }
+    choice = wgetch(menu);
 
-void print_menu() {
-  //to do
+    switch (choice) {
+    case KEY_UP:
+      highlight--;
+      if (highlight == -1)
+        highlight= 0;
+      break;
+    case KEY_DOWN:
+      highlight++;
+      if (highlight == 4)
+        highlight = 3;
+      break;
+    default:
+      break;
+    }
+    if (choice == 10)
+      break;
+  }
+
+  switch(highlight) {
+  case 3:
+    quit();
+    break;
+  case 2:
+    //save as
+    break;
+  case 1:
+    //save
+    break;
+  case 0:
+    //open
+    break;
+  }
+
+  touchwin(text_border);
+  refresh();
 }
-*/
