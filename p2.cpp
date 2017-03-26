dbe386307b45ce13e666d805b269e75e150bcf5a
viziad <mtbeale@hotmail.com>
2017-03-26 13:57:06 -0400

    Update p2.cpp
    Added open file (with + without file in CL as argument) and save as(doesn't erase old file)(the typed text for the filename in the menu window isn't visible). Still need to update filename in bottom left corner

--- p2.cpp
+++ p2.cpp
@@ -217,12 +217,21 @@ void showMenu() {
     }
 
     switch (highlight) {
+        char str[80];
         case 0: // Open
+            mvwprintw(menu,8,2,"Enter file name: ");
+            wgetstr(menu,str);
+            wrefresh(menu);
+            ed.openFile(str);
             break;
         case 1: // Save
             ed.saveFile();
             break;
         case 2: // Save As
+            mvwprintw(menu,8,2,"Save file as: ")
+            wgetstr(menu,str);
+            wrefresh(menu);
+            ed.saveFileAs(str);
             break;
         case 3: // Quit
             doExit = true;
@@ -238,81 +247,3 @@ static void quit() {
     exit(0);
 }
 
-//I commented this out because i'm still working on implementing it differently,
-//but the code should be mostly correct
-
-/*
-void create_menu() {
-
-  // Menu choices
-  const char *choices[] = {
-    "Open",
-    "Save",
-    "Save As",
-    "Exit",
-  };
-
-  ITEM **my_items;
-  int c;
-  MENU *my_menu;
-  WINDOW *my_menu_win;
-  int n_choices, i;
-
-  //creates/allocates choices to the menu
-  n_choices = ARRAY_SIZE(choices);
-  my_items = (ITEM **)calloc(n_choices, sizeof(ITEM *));
-  for(i = 0; i < n_choices; ++i)
-    my_items[i] = new_item(choices[i]);
-
-  //creates the actual menu
-  my_menu = new_menu((ITEM **)my_items);
-
-  //creates the window for the menu
-  my_menu_win = newwin(10,40,4,4);
-  keypad(my_menu_win,TRUE);
-
-
-  set_menu_win(my_menu, my_menu_win);
-  set_menu_sub(my_menu, derwin(my_menu_win, 6, 38, 3, 1));
-
-  //Menu mark is *(aka * will appear next to the selected menu option)
-  set_menu_mark(my_menu, " * ");
-
-  //Print a border around the menu window
-  box(my_menu_win, 0, 0);
-  print_in_middle(my_menu_win, 1, 0, 40, "My Menu");
-  mvwaddch(my_menu_win, 2, 0, ACS_LTEE);
-  mvwhline(my_menu_win, 2, 1, ACS_HLINE, 38);
-  mvwaddch(my_menu_win, 2, 39, ACS_RTEE);
-  refresh();
-
-  //post menu to screen
-  post_menu(my_menu);
-  wrefresh(my_menu_win);
-
-
-  while((c = wgetch(my_menu_win)) != KEY_F(1))
-    {       switch(c)
-        {case KEY_DOWN:
-            menu_driver(my_menu, REQ_DOWN_ITEM);
-            break;
-  case KEY_UP:
-          menu_driver(my_menu, REQ_UP_ITEM);
-          break;
-        }
-      wrefresh(my_menu_win);
-    }
-
-  //free previously allocated memory
-  unpost_menu(my_menu);
-  free_menu(my_menu);
-  for (i =0; i < n_choices; i++)
-    free_item(my_items[i]);
-  endwin();
-}
-
-
-void print_menu() {
-  //to do
-}
-*/
